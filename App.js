d06e169eab7b6903440b419b8a5949a077329908
Dream-Arcade <thinkingsingularity@gmail.com>
2024-09-14 22:27:27 -0500

    test 4

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "green",
+    backgroundColor: "red",
     alignItems: "center",
     justifyContent: "center",
   },
